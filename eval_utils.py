7a4a3685d41039410fa59a2d53b7ffbc45b5f86c
miguelag99 <miguelag99@hotmail.com>
2021-06-30 19:46:42 +0200

    3DIoU, 2D IoU added

--- eval_utils.py
+++ eval_utils.py
@@ -1,6 +1,9 @@
 import math
+import numpy as np
 
 from shapely.geometry import Polygon
+
+from geometric_utils import create_3d_bbox, calc_distance_2d
 
 
 class eval_object():
@@ -13,22 +16,32 @@ class eval_object():
         self.alpha = alpha
         self.rotation = rotation
         self.timestamp = timestamp
+        self.distance = 100
+        self.status = 0
 
     def __str__(self):
-        return("Frame:{} Id:{} Loc:{}".format(self.frame,self.id,self.loc))
+        return("Frame:{} Id:{} Loc:{} Status:{}".format(self.frame,self.id,self.loc,self.status))
 
-    def asociate_detected_object(self,object):
+    def asociate_detected_object(self,object,dis):
 
-        #Only used in gt objects to save the correspondent detection
+        # Only used in gt objects to save the correspondent detection
+        # We use the minimum distance to asociate the ground-truth with a detection
 
-        if object.frame == self.frame:
-
+        if dis < self.distance: 
             self.det_id = object.id
             self.det_bbox2d = object.bbox2d
             self.det_dim = object.dim
             self.det_loc = object.loc
             self.det_alpha = object.alpha
             self.det_rotation = object.rotation
+            self.distance = dis
+            self.set_status()
+            return 1
+        else:
+            return 0
+  
+    def set_status(self):
+        self.status = 1
   
 
 def iou_2d(box1,box2):
@@ -52,16 +65,35 @@ def iou_2d(box1,box2):
     poly2 = Polygon([a2,b2,c2,d2])
 
     iou = poly1.intersection(poly2).area / poly1.union(poly2).area
-
-    return iou
+    intersect_polygon = poly1.intersection(poly2).exterior.coords[:-1]
+    return iou, intersect_polygon
  
-        
 
+def iou_dist_3d(obj1,obj2):
 
-def calc_distance(p1,p2):
+    # Both obj1 and obj2 are eval_object class elements
 
-    # p1 and p2 are touples of cordinates, in camera (x,z) in lidar (x,y)
+    box3d_1 = create_3d_bbox(obj1)
+    box3d_2 = create_3d_bbox(obj2)
+    bird_poly1 = Polygon([(box3d_1[0,0],box3d_1[1,0]),(box3d_1[0,1],box3d_1[1,1]),(box3d_1[0,2],box3d_1[1,2]),(box3d_1[0,3],box3d_1[1,3])])
+    bird_poly2 = Polygon([(box3d_2[0,0],box3d_2[1,0]),(box3d_2[0,1],box3d_2[1,1]),(box3d_2[0,2],box3d_2[1,2]),(box3d_2[0,3],box3d_2[1,3])])
 
-    d = math.sqrt(math.pow(p1[0]-p2[0],2)+math.pow(p1[1]-p2[1],2))
+    #bird_inters_poly = bird_poly1.intersection(bird_poly2).exterior.coords[:-1]
+    bird_inters = bird_poly1.intersection(bird_poly2).area
+    
+    low = max([box3d_1[2,0],box3d_2[2,0]])
+    top = min([box3d_1[2,4],box3d_2[2,4]])
+    h = top-low
 
-    return d
+    inter_volume = bird_inters*h
+
+    vol1 = abs(box3d_1[2,0]-box3d_1[2,4]) * bird_poly1.area
+    vol2 = abs(box3d_2[2,0]-box3d_2[2,4]) * bird_poly2.area
+
+    iou = inter_volume/(vol1+vol2-inter_volume)
+
+    d = calc_distance_2d((obj1.loc[0],obj1.loc[1]),(obj2.loc[0],obj2.loc[1]))
+ 
+    return iou,d
+    
+
--- evaluate.py
+++ evaluate.py
@@ -1,11 +1,18 @@
 import io
+import time 
 import os
+import numpy as np
 
 from file_utils import get_csv
-from eval_utils import iou_2d, calc_distance
+from eval_utils import iou_2d, iou_dist_3d
+
 
 FILE_NAME = "/UwU.csv"
 GT_FILE = "/UwU_gt.csv"
+
+DIST_THRESHOLD = 1
+IOU2D_THRESHOLD = 0.5
+
 
 
 def main():
@@ -13,21 +20,46 @@ def main():
     elements = get_csv(os.getcwd()+ FILE_NAME)
     ground_tr = get_csv(os.getcwd()+ GT_FILE)
     
-
     # Iterations with gt instead of detections
 
     for gt in ground_tr:
 
-        print("Gt:{}".format(gt))
+        print("Gt:{}".format(gt)+os.linesep)
 
         for el in elements:
 
-            print(el)
+            iou2d, poly = iou_2d(gt.bbox2d,el.bbox2d)
+            iou3d, distance = iou_dist_3d(gt,el)
 
-            iou2d = iou_2d(gt.bbox2d,el.bbox2d)
-            print(iou2d)
+            if distance < DIST_THRESHOLD and iou2d > IOU2D_THRESHOLD: ##Falta asociar con timestamp
 
-        #gt.asociate_detected_object(object=)
+                i = gt.asociate_detected_object(el,distance)
+
+
+    #Arreglar
+    #El numero de status 1 en gt debe ser igual que el de detections, status en detetcions no es necesario
+
+    # Status de cada objeto de forma paralela
+
+    f = lambda x: x.status
+    vectorized_f = np.vectorize(f)
+
+    status_vector = np.sum(vectorized_f(ground_tr))
+    print(gt_status)
+
+ 
+
+    for gt in ground_tr:
+
+        print("Gt:{}".format(gt)+os.linesep)
+
+    for det in elements:
+
+        print("Det{}".format(det)+os.linesep)
+
+
+
+      
 
 
 
